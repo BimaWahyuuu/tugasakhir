628db66d2c55234d566928ed2a63c9fccdd13344
firmanhoy <firmanhoys@gmail.com>
2023-01-08 11:24:09 +0700

    membuat relasi dan mengubah gitignore

--- app/Models/Waka.php
+++ app/Models/Waka.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Bidang;
 
 class Waka extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+
+    public function bidang(){
+        return $this->hasMany(Bidang::class, 'waka_id', 'id');
+    }
+    
+
+    protected $table = 'waka';
 }
