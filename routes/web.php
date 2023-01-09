af02fd42f8d8fb5dfc0327af231bc2bc16e5905f
Bima Wahyu Luckyta <bimawahyu11rpl1@gmail.com>
2023-01-09 13:13:33 +0700

    integrasi template landing

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function(){
+    return view('user.index');
+});
+
 Route::get('/masuk', [LoginController::class, 'main'])->name('masuk')->middleware('guest');
 Route::get('/masuk', [LoginController::class, 'main'])->name('masuk')->middleware('guest');
 Route::post('/masuk', [LoginController::class, 'login'])->name('masuk.login')->middleware('guest');
